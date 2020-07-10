82f102b7b3d0d1fd2f8f6e68a0d31e83fe3a0b66
xboxtracer <xboxtracer@yandex.ru>
2020-07-10 07:55:38 +0300

    Страница входа выполнена, регистрация и логинизация проводятся успешно

---
+++ public/homePage.js
@@ -0,0 +1,4 @@
+
+const logoutUser = new LogoutButton();
+// setTimeout(ApiConnector.logout(location.reload()), 5000)
+// logoutUser.logoutClick()
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,2 +1,10 @@
 'use strict'
 
+const newUser = new UserForm();
+newUser.loginFormCallback = data => ApiConnector.login(data, response => {
+    response.success ? location.reload() : newUser.setLoginErrorMessage(response.data), newUser.loginErrorMessageBox;
+});
+newUser.registerFormCallback = data => ApiConnector.register(data, response => {
+    response.success ? location.reload() : newUser.setRegisterErrorMessage(response.data), newUser.registerErrorMessageBox;
+});
+
